e63cd7b8b868a2679fb6fc10e1dce3073eb65827
DennisMaas <mail@dennismaas.de>
2020-10-15 12:42:07 +0200

    add useState to make an array add characters.map do make a div containing all characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 
 export default function CharacterList(){
+    const [characters, setCharacters] = useState([]);
     useEffect(()=> {
        let url = "https://rickandmortyapi.com";
        axios.get(url + "/api/character")
-           .then(console.log);
+           .then((response) =>
+               setCharacters(response.data.results))
+
     }, []);
     return (<div>
-
-
+        {
+            characters.map(character => <div>{character.name}</div>)
+        }
     </div>)
 }
